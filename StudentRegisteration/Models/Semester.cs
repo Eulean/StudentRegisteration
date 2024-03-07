78ed5f7295ac00faf9195761dbbc89ba1bcdad22
Eulean <eulen9t5@gmail.com>
2024-03-07 15:15:05 +0630

    By Eulean adding Student, Sememster and Subject Models

---
+++ StudentRegisteration/Models/Semester.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentRegisteration.Models
+{
+    public class Semester
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
---
+++ StudentRegisteration/Models/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudentRegisteration.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [Display(Name = "Phone Number")]
+        public string Phone {  get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [Display(Name = "NRC Number")]
+        public string NRC { get; set; }
+
+        [Required]
+        public DateTime RegDate { get; set; } = DateTime.Now;
+    }
+}
